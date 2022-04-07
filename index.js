3152321d214aafbc9ed9b8a65151068709bd3686
Joseph Rubino <joeyrubi22@gmail.com>
2022-04-07 11:07:30 -0700

    mongoose pt 3

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Campsite = require('./models/campsite');
 const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
@@ -17,11 +18,27 @@ connect.then(() => {
         description: 'test'
     })
     .then(campsite => {
-        console.log(campsite);
-        return Campsite.find()
+        console.log(campsite)
+
+        campsite.comments.push({
+            rating: 5,
+            text: 'What a magnificant view!',
+            author: 'Tinus Lorvaldes'
+        });
+
+        return campsite.save()
     })
-    .then(campsites => {
-        console.log(campsites);
+    .then(campsite => {
+        console.log(campsite);
+
+        return Campsite.findByIdAndUpdate(campsite._id, {
+            $set: { description: 'Updated Test Document' }
+        }, {
+            new: true
+        })
+    })
+    .then(campsite => {
+        console.log(campsite);
         return Campsite.deleteMany();
     })
     .then(() => {
